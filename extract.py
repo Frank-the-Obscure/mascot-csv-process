c467098cc6df680bcc36e614d89b7a9c1de042f8
Frank Hu <humour114@gmail.com>
2015-06-23 20:03:07 +0800

    using try-except-else to be pathonic

--- extract.py
+++ extract.py
@@ -3,21 +3,38 @@
 
 
 import re
-from sys import argv
 
-# read one hand infomation
-script, filename = argv
-txt = open(filename)
-data = txt.read()
+combine = [] #combined protein list
 
-test_info = re.findall('\d+,.*?\n', data)
+while True:
+    filename = input('Please input file name (without ".csv"), input s to finish: ')
+    print(filename)
+    if filename == "s":
+        # end and output
+        try:
+            output = open('combined.csv', 'w')
+        except PermissionError:
+            print('permission denied, please close the file to write.')
+        else:
+            for i in combine:
+                output.write(i)
+            break            
+    else:
+        # read file infomation
+        try:
+            csv = open(filename + '.csv')
 
-samplename = re.findall('(?<=Peak list data path,).*',data)[0] # get raw file name in csv
-print samplename #test
+        except FileNotFoundError:
+            print ('File not found!')
+        else:
+            print ('enter name ok')
+            data = csv.read()
+            protein_list = re.findall('\d+,.*?\n', data)
 
-output = open('%s_extracted.csv'%(filename), 'w')
-#print test_info
-for i in test_info:
-	if re.findall('OS=', i): # use of uniprot-formated file
-		output.write(samplename + ',')
-		output.write(i)
+            samplename = re.findall('(?<=Peak list data path,).*',data)[0] # get raw file name in csv line 9
+            print(samplename) #test
+            for i in protein_list:
+                if re.findall('OS=', i): 
+                    combine.append(samplename + ',' + i)
+            print(len(combine))
+
